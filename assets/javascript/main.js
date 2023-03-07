8e084345d8a8135129abcccb03b4ce753192d483
fabiodelli <ntqdel@hotmail.com>
2023-03-07 14:57:54 +0100

    add link and script code

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+// Richiesta nome, cognome e colore preferito all'utente
+let nome = prompt("Inserisci il tuo nome:");
+let cognome = prompt("Inserisci il tuo cognome:");
+let colorePreferito = prompt("Inserisci il tuo colore preferito:");
+
+// Concatenazione delle tre stringhe
+let risultato = nome + cognome + colorePreferito;
+
+// Visualizzazione del risultato nel DOM
+document.write("La tua password è: " + risultato);
